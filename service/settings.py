24ab3788c5eeae8296d2407da2444d8764694f8b
Toktorov <ktoktorov2005@gmail.com>
2022-04-10 16:32:14 +0600

    cors

--- service/settings.py
+++ service/settings.py
@@ -71,6 +71,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
