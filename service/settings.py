64f787cba496bfcf9b7f17c096bcd512a7bea7ca
Toktorov <ktoktorov2005@gmail.com>
2022-05-22 23:33:57 +0600

    config

--- service/settings.py
+++ service/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dp)(li%9^^l-#&ow3itj6@@bip^*gn5jcn3zxvw*4=pq^b3q_o'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,9 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIT_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kyzmat24@gmail.com'
-EMAIL_HOST_PASSWORD = 'vgciwtdmopvvbpsm'
-
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
