dc0e3dbd71c4660cc8264fddda5f8bcef3ab83e7
Toktorov <ktoktorov2005@gmail.com>
2022-05-23 00:04:37 +0600

    testing new branch

--- service/settings.py
+++ service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 #Tested branch
+#Checked
 import os
 from pathlib import Path
 from datetime import timedelta
