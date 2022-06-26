104815562f0de35b51e4c2e45c657c5f3c378426
Toktorov <ktoktorov2005@gmail.com>
2022-06-26 12:27:39 +0600

    admin config

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -4,7 +4,7 @@ from apps.users.models import User, Contact, Media
 # Register your models here.
 class UserFilterAdmin(admin.ModelAdmin):
     list_filter = ('username', )
-    list_display = ('username',  'description', 'is_active', 'is_staff')
+    list_display = ('username',  'description', 'verifed', 'is_active', 'is_staff', 'status_user', 'another')
     search_fields = ('username', )
 
 admin.site.register(User, UserFilterAdmin)
