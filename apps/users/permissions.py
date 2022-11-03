ac59dcda1bb7af1028717a64536f4ca7a9571c26
Toktorov <ktoktorov2005@gmail.com>
2022-11-03 13:33:57 +0600

    update permissions

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -9,12 +9,12 @@ class UserPermissions(permissions.BasePermission):
 
 class UserContactPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if obj.user == request.user:
             return True
-        return obj.user == request.user
+        return False
     
 class UserMediaPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if obj.user == request.user:
             return True
-        return obj.user == request.user
+        return False
